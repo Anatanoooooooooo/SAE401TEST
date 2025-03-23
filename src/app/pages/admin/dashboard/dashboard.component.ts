8976a51be86f633dd90b14271e8159172bd353b6
Anatanoooooooooo <lidrasyt@gmail.com>
2025-03-23 22:19:22 +0100

    git pull

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -10,36 +10,5 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
 
-
-  nomProfile: string = "Nom profile";
-
-  nom: string = "";
-  prenom: string = "";
-  mail: string = "";
-  adresse: string = "";
-  codePostale: string = "";
-  telephone: string = "";
-  login: string = "";
-  password: string = "";
-
-  constructor(private router: Router) {}
-
-  confirmer() {
-    console.log("Données sauvegardées :", {
-      nom: this.nom,
-      prenom: this.prenom,
-      mail: this.mail,
-      adresse: this.adresse,
-      codePostale: this.codePostale,
-      telephone: this.telephone,
-      login: this.login,
-      password: this.password
-    });
-    alert("Informations confirmées !");
-  }
-
-  retour() {
-    this.router.navigate(['/']);
-  }
 }
 
