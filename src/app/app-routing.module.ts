4ae6205a579b53dbd02bc2418de5cfcb9294c47e
Anatanoooooooooo <lidrasyt@gmail.com>
2025-03-23 22:44:39 +0100

    git pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { AuthGuard } from './services/auth-guard.service';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AjoutProfileComponent } from './pages/admin/ajout-profile/ajout-profile.component';
+import { ModifierProfileComponent } from './pages/admin/modifier-profile/modifier-profile.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'ajout-profile', component: AjoutProfileComponent, canActivate: [AuthGuard] },
+  { path: 'modifier-profile', component: ModifierProfileComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent},
   { path: '**', redirectTo: '/home' } // Redirection par défaut
 ];
--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -10,5 +10,17 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
 
+  profiles = [1, 2, 3, 4, 5, 6, 7, 8]; // Exemple de données
+
+  editProfile(profile: number) {
+    // Redirection vers admin.html
+    window.location.href = '/modifier-profile';
+  }
+
+  deleteProfile(profile: number) {
+    alert(`Supprimer le profil: ${profile}`);
+  }
+
+
 }
 
